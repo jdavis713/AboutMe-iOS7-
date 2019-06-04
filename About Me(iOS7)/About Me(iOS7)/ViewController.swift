9726741b0e7dce5d3c9b6496b8c4a33fecbb3200
Jordan Davis <jordan.m.davis92@gmail.com>
2019-06-04 12:14:22 -0400

    initial commit
    added print statement

---
+++ About Me(iOS7)/About Me(iOS7)/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  About Me(iOS7)
+//
+//  Created by Jordan Davis on 6/4/19.
+//  Copyright © 2019 Jordan Davis. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print("My name is Jordan Davis")
+    }
+
+
+}
+
